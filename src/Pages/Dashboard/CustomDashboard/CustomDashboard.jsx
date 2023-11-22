6be8c0a579f67970709a24f5f69aa43fa659f8d9
Ahmed-Nasser99 <ahmednsmd@gmail.com>
2023-11-22 10:33:47 +0200

    Set Shared Modal To View Image In Fullscreen

--- src/Pages/Dashboard/CustomDashboard/CustomDashboard.jsx
+++ src/Pages/Dashboard/CustomDashboard/CustomDashboard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import customDashboard from "../../../assets/Images/Dashboard/CustomDashboard.jpeg";
+import Image from "../../../Components/Image/Image";
 
 export default function CustomDashboard() {
   return (
     <div className="w-100 d-flex align-items-center justify-content-center flex-column">
       <h1 className="text-danger">CustomDashboard</h1>
-      <img src={customDashboard} alt="Main Dashboard" className="rounded" />
+      <Image
+        src={customDashboard}
+        alt={"Main Dashboard"}
+        className={"rounded"}
+      />
     </div>
   );
 }
--- src/Pages/Dashboard/MainDashboard/MainDashboard.jsx
+++ src/Pages/Dashboard/MainDashboard/MainDashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import mainDashboard from "../../../assets/Images/Dashboard/MainDashboard.jpeg";
+import Image from "../../../Components/Image/Image";
 export default function MainDashboard() {
   return (
     <div className="w-100 d-flex align-items-center justify-content-center flex-column">
       <h1 className="text-success">MainDashboard</h1>
-      <img src={mainDashboard} alt="Main Dashboard" className="rounded" />
+      <Image src={mainDashboard} alt={"Main Dashboard"} className={"rounded"} />
     </div>
   );
 }
