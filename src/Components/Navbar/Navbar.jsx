d829c1613b69c95c7e1e3d7afb0f9b2f3c280b1c
a.nasserr1 <a.nasserr1@outlook.com>
2024-01-13 14:41:56 +0200

    Add Localization To Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import logo from "../../assets/images/avms logo02-01.png";
+import en from "../../assets/Images/Icons/en.png";
+import ar from "../../assets/Images/Icons/ar.png";
 import { useTranslation } from "react-i18next";
 export default function Navbar({ setDisplaySidebar, DisplaySidebar }) {
   let Navigate = useNavigate();
@@ -13,33 +14,38 @@ export default function Navbar({ setDisplaySidebar, DisplaySidebar }) {
 
   return (
     <nav className="navbar bg-white position-relative fixed-top w-100 ">
-      <div className="container">
-        <div className="d-flex align-items-center justify-content-between mx-3 w-100">
-          <Link className="navbar-brand" href="/">
-            <img
-              src="https://egydns.ddns.net:1488/Images/Icons/avms-logo02-01.ico"
-              alt="AVMS"
-              width="110"
-              height="40"
-            />
-          </Link>
-          <div className="select d-flex align-items-center gap-3">
-            {i18n.language == "ar" ? (
+      <div className="d-flex align-items-center justify-content-between mx-4  w-100">
+        <Link className="navbar-brand" href="/">
+          <img
+            src="https://egydns.ddns.net:1488/Images/Icons/avms-logo02-01.ico"
+            alt="AVMS"
+            width="110"
+            height="40"
+          />
+        </Link>
+        <div className="searchInput position-relative w-50">
+          <span className="search_icon">
+            <i class="fa-brands fa-searchengin "></i> Search
+          </span>
+          <input
+            class="form-control mr-sm-2 rounded-5"
+            type="search"
+            aria-label="Search"
+          />
+        </div>
+        <div class="dropdown">
+          <button
+            class="btn btn-white dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img src={i18n.language === "ar" ? ar : en} alt="arabic_Logo" />
+          </button>
+          <ul class="dropdown-menu">
+            <li>
               <button
-                className="btn btn-outline-info"
-                onClick={() => {
-                  i18n.changeLanguage("en");
-                  document
-                    .getElementsByTagName("html")[0]
-                    .setAttribute("dir", "ltr");
-                }}
-              >
-                English
-                <i className="fa-solid fa-earth-americas mx-2"></i>
-              </button>
-            ) : (
-              <button
-                className="btn btn-outline-info"
+                class="dropdown-item"
                 onClick={() => {
                   i18n.changeLanguage("ar");
                   document
@@ -47,19 +53,25 @@ export default function Navbar({ setDisplaySidebar, DisplaySidebar }) {
                     .setAttribute("dir", "rtl");
                 }}
               >
-                عربي
-                <i className="fa-solid fa-earth-americas mx-2"></i>
+                <img src={ar} alt="arabic_Logo" />
+                <span className="mx-2 fw-medium">Arabic</span>
               </button>
-            )}
-            {/* <button
-            className="btn btn-outline-info"
-            onClick={() => {
-              toggleFullScreen();
-            }}
-          >
-            {t("signOut")}
-          </button> */}
-          </div>
+            </li>
+            <li>
+              <button
+                class="dropdown-item"
+                onClick={() => {
+                  i18n.changeLanguage("en");
+                  document
+                    .getElementsByTagName("html")[0]
+                    .setAttribute("dir", "ltr");
+                }}
+              >
+                <img src={en} alt="english_Logo" />
+                <span className="mx-2 fw-medium">English</span>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
